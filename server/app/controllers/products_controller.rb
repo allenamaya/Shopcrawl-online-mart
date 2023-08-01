5a536318d0357321209586d14b77efbd5bf42956
John Kipruto <johnkipruto157@gmail.com>
2023-08-01 15:05:28 +0300

    Add scrapers for jumia, jiji, ebay, aliexpress

--- server/app/controllers/products_controller.rb
+++ server/app/controllers/products_controller.rb
@@ -1,18 +1,18 @@
 class ProductsController < ApplicationController
 
     def search_products
-        search_term = params[:search_term]
-        puts search_term
+        search_term = params[:search_term].split(' ').join
+        # search_term = 'tv'
         products = scrape_sites(search_term)
         render json: products, status: :ok 
     end
 
     def scrape_sites(search_term)
         jumia_products = scrape_jumia(search_term)
-        jiji = scrape_jiji(search_term)
-        ebay_products = scrape_ebay(search_term)  
-        
-        return [*ebay_products, *jiji, *jumia_products]
+        jiji = scrape_jiji(search_term) 
+        ebay_products = scrape_ebay(search_term) 
+        aliexpress_products = scrape_aliexpress(search_term)
+        return [*jumia_products, *jiji,*ebay_products, *aliexpress_products]
     end
 
     def scrape_ebay(search_term) 
@@ -20,14 +20,17 @@ class ProductsController < ApplicationController
         doc = Nokogiri::HTML(html)
         items = doc.css('.s-item')
         items = items.map do |item|
+    
             {   
                 site: "EBAY",
                 image: item.css('.s-item__image-wrapper img').map{|elm| elm['src']}[0],
                 title: item.css('.s-item__title span').text,
                 sub_title: item.css('.s-item__subtitle').text,
-                price: item.css('.s-item__price').text.split('$')[1].to_i,
-                shipping_cost: item.css('.s-item__shipping').text.split(" ")[0].to_s.split("$")[1].to_i
+                price: item.css('.s-item__price').text.split('$')[1].to_i * 142,
+                shipping_cost: item.css('.s-item__shipping').text.split(" ")[0].to_s.split("$")[1].to_i,
+                link: item.css('a').map{|elem| elem['href']}[0]
             }
+        
         end
     end
 
@@ -39,7 +42,10 @@ class ProductsController < ApplicationController
         items = items.map do |item|
             {   
                 site: "JUMIA",
-                image: item.css('.img-c img').map{|elem| elem["data-src"]}[0]
+                image: item.css('.img-c img').map{|elem| elem["data-src"]}[0],
+                title: item.css('div.info > h3.name').text,
+                price: item.css('div.info > .prc').text.to_s.split(" ")[1].to_s.split(',').join('').to_i,
+                link: "https://www.jumia.co.ke".to_s + item.css('a').map{|elem| elem['href']}[0].to_s  
             }
         end
         return items
@@ -55,21 +61,24 @@ class ProductsController < ApplicationController
             {   
                 site: "JIJI",
                 title: item.css('.b-advert-title-inner').text,
-                image: item.css('.b-list-advert-base__img picture > source').map {|elem| elem['srcset']}[0]
+                image: item.css('.b-list-advert-base__img picture > source').map {|elem| elem['srcset']}[0],
+                price: item.css('.qa-advert-price').text.split(" ")[1].split(',').join.to_i,
+                link: "https://jiji.co.ke".to_s + item.css('a').map{|elm| elm['href']}[0].to_s,
             }
         end
         return items
     end
 
-    def scrape_shopit(search_term)
-        html = URI.open("https://shopit.co.ke/?match=all&subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&q=#{search_term}&dispatch=products.search&security_hash=6908dd946302968b4d27bcdb74c7e7c2")
+    def scrape_aliexpress(search_term)
+        html = URI.open("https://www.aliexpress.com/w/wholesale-#{search_term}.html")
         doc = Nokogiri::HTML(html)
-        items = doc.css('.ut2-gl__body')
+        items = doc.css('.search-card-item')
         items = items.map do |item|
             {   
-                site: "SHOPIT",
-                title: item.css('.ut2-gl__name').text,
-                image: item.css('.ut2-gl__image img').map{|elm| elm['data-src']}[0]
+                title: item.css('h1').text,
+                image: item.css('img').map{|elm| elm['src']}[0],
+                link: item['href'].to_s,
+                price: item.css('div.manhattan--price-sale--1CCSZfK').text.split('KES')[1].split(',').join.to_i
             }
         end
     end
