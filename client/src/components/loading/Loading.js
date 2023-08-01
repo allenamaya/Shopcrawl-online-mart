c1e8ac26da96f49440c619e7d05b8471d52cf6e8
John Kipruto <johnkipruto157@gmail.com>
2023-08-01 14:56:35 +0300

    Add loading page for search action

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -1,17 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext} from 'react'
 import ProductsContext from '../context/ProductsContext'
 import { useNavigate } from 'react-router-dom'
 
 const Loading = () => {
     const {products} = useContext(ProductsContext)
     const navigate = useNavigate()
-
-  return (
-    <div>
-        <h1> Fetching your results................</h1>
-        {products ? navigate('/search') : null }
-    </div>
-  )
+      return (
+        <div>
+          <h1>Getting your products</h1>
+          {products ? navigate("/products") : null}
+        </div> 
+    )   
 }
 
 export default Loading
