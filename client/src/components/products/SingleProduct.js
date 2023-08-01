8b9ef5794d2a5aaf2ac63f46f6fb0c2b52b1469e
John Kipruto <johnkipruto157@gmail.com>
2023-08-01 14:58:43 +0300

    Create card for single product

--- client/src/components/products/SingleProduct.js
+++ client/src/components/products/SingleProduct.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const SingleProduct = ({product}) => {
-  return (
-    <Card style={{width: '18rem'}}>
-        <Card.Img src={product.image}>
+    if(product.price > 50){
+      return (
+        <Link to={product.link} target='_blank'>
+          <Card style={{width: '18rem'}} className='product'>
+            <Card.Img src={product.image} className='product-img'>
+            </Card.Img>
+            <Card.Body>
+            <h5>{product.title}</h5>
+            <h5>{product.site}</h5>
+            <h4>Ksh {product.price}</h4>
+            </Card.Body>
+        </Card>
+        </Link>
+        
+      )
+    }
+      
 
-        </Card.Img>
-        <Card.Body>
-        <h5>{product.title}</h5>
-        <h5>{product.site}</h5>
-        </Card.Body>
-    </Card>
-  )
-}
+  }
+  
 
 export default SingleProduct
