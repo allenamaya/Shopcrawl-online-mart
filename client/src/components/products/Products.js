2f16277e355033ca2cd8ffa7f09f213d0c768ea5
John Kipruto <johnkipruto157@gmail.com>
2023-08-01 14:58:12 +0300

    Add products display section

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react'
+import { Container } from 'react-bootstrap'
 import SingleProduct from './SingleProduct'
 import ProductsContext from '../context/ProductsContext'
 
 const Products = () => {
   const {products} = useContext(ProductsContext)
   return (
-    <div className='products'>
+    <Container>
+       <div className='products'>
         {products.map(product => <SingleProduct product={product} />)}
     </div>
+    </Container>
+   
   )
 }
 
