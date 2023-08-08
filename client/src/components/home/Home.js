78f60e7d2e38db8b8c29dafbd632c98b35dbaa79
allenamaya <ahlee.amaya@gmail.com>
2023-08-08 14:01:16 +0300

    Updated Homepage

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -3,14 +3,33 @@ import Stores from './stores/Stores'
 import HomeProducts from './home_products/HomeProducts'
 import { Container } from 'react-bootstrap'
 import { International, InHome, InBeauty } from './frequently_purchased'
-
+import Carousel from 'react-bootstrap/Carousel';
+import Welcome from '../../images/banner1.png'
+import Customer from '../../images/banner2.png'
+import Weekly from '../../images/banner3.png'
+import Onsite from '../../images/banner4.png'
 
 
 const Home = ({products}) => {
   return (
       <div className='home'>
         <div className='banner home-banner'>
-          <h1>Welcome to Shopcrawl!</h1>
+    <Carousel fade interval={1200}>
+      <Carousel.Item>
+        <img src={Welcome} alt='' />
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Weekly} alt='' />
+       
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Customer} alt='' />
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Onsite} alt='' />
+      </Carousel.Item>
+    </Carousel>
+ 
         </div>
        <div>
        <Container>
--- client/src/components/home/home_products/SingleProduct.js
+++ client/src/components/home/home_products/SingleProduct.js
@@ -2,10 +2,48 @@ import React, { useContext } from 'react'
 import { Card } from 'react-bootstrap'
 import {useNavigate, Link } from 'react-router-dom'
 import ProductsContext from '../../context/ProductsContext'
+import Snacks from '../../../images/frame8.png'
+import Toys from '../../../images/Frame9.png'
+import Groceries from '../../../images/Frame10.png'
+import Gaming from '../../../images/frame11.png'
+ import Fashion from '../../../images/frame12.png'
+ import Dining from '../../../images/frame13.png'
+ import Electronics from '../../../images/frame14.png'
+ import Health from '../../../images/frame15.png'
+ import Cosmetics from '../../../images/frame16.png'
+ import Fitness from '../../../images/frame17.png'
+ 
 
 const SingleProduct = ({fProduct}) => {
   const navigate = useNavigate()
   const {setProducts} = useContext(ProductsContext)
+
+  let imageSource = null;
+
+  if (fProduct.name ==="fashion"){
+    imageSource = Fashion
+  }
+  else if(fProduct.name ==="dining"){
+    imageSource = Dining
+  }  else if(fProduct.name ==="electronics"){
+    imageSource = Electronics
+  }  else if(fProduct.name ==="health"){
+    imageSource = Health
+  }  else if(fProduct.name ==="gaming"){
+    imageSource = Gaming
+  }  else if(fProduct.name ==="cosmetics"){
+    imageSource = Cosmetics
+  }  else if(fProduct.name ==="toys"){
+    imageSource = Toys
+  }  else if(fProduct.name ==="grocery"){
+    imageSource = Groceries
+  }  else if(fProduct.name ==="snacks"){
+    imageSource = Snacks
+  }else if(fProduct.name ==="fitness"){
+    imageSource = Fitness
+  }else{
+    imageSource = fProduct.image
+   }
 
  
 async function searchItem(){
@@ -34,10 +72,11 @@ async function searchItem(){
   return (
     <Link onClick={searchItem} to={'/loading'} className='fProduct-link'>
       <Card className='home-product' >
-          <Card.Img src={fProduct.image} ></Card.Img>
-          <Card.Body>
+
+          <Card.Img src={imageSource} ></Card.Img>
+          {/* <Card.Body>
             <Card.Title>{fProduct.name}</Card.Title>
-          </Card.Body>
+          </Card.Body> */}
       </Card>
     </Link>
     
